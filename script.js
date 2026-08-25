6255d5212548641479fc495de53a3ab84286f1e6
luciano <lucianojuniodacostafonseca@gmail.com>
2026-08-25 18:31:33 -0300

    algumas alterações leves no js

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 // MANIPULANDO A ORIGEM E O RESUMO ORIGEM
-const inputOrigem = document.getElementById("origem")
-const resumoOrigem = document.getElementById("resumo-origem")
+const inputOrigem = document.getElementById("origem");
+const resumoOrigem = document.getElementById("resumo-origem");
 
 inputOrigem.addEventListener("input", () => {
     if (inputOrigem.value.trim() !== "") {
         resumoOrigem.innerText = inputOrigem.value;
     } else {
-        resumoOrigem.innerText = "-"
+        resumoOrigem.innerHTML = "-";
     }
-})
+});
+
 
 // MANIPULANDO O DESTINO E O RESUMO DESTINO
 const inputDestino = document.getElementById("destino");
@@ -18,58 +19,68 @@ inputDestino.addEventListener("input", () => {
     if (inputDestino.value.trim() !== "") {
         resumoDestino.innerText = inputDestino.value;
     } else {
-        resumoDestino.innerText = "-"
+        resumoDestino.innerText = "-";
     }
 });
 
 
-//MANIPULANDO A DISTÂNCIA E SEUS VALORES FIXOS E VARIÁVEIS
+// MANIPULANDO A DISTÂNCIA E SEUS VALORES FIXOS E VARIÁVEIS
 const inputDistancia = document.getElementById("distancia");
-const resumoDistancia = document.getElementById("resumo-distancia")
+const resumoDistancia = document.getElementById("resumo-distancia");
+
 
 inputDistancia.addEventListener("change", () => {
     const km = Number(inputDistancia.value);
 
-    if(km > 0) {
-        const fixo = 50;
-        const valorPorKm = 4;
-        const total = fixo + (km * valorPorKm);
+    const fixo = 50;
+    const variavel = 4;
+    const total = fixo + (km * variavel)
 
-        console.log(`O valor aproximado é R$ ${total}`)
+    console.log(`O valor aproximado é R$ ${total}`)
 
+    if (km > 0) {
         resumoDistancia.innerText = inputDistancia.value;
+    } else {
+        resumoDistancia.innerText = "-";
     }
-})
+});
 
 
-//MANIPULANDO A DESCRIÇÃO DOS ITENS
-const itens = document.getElementById("itens")
-const resumoItens = document.getElementById("resumo-itens")
+// MANIPULANDO A DESCRIÇÃO DOS ITENS
+const itens = document.getElementById("itens");
+const resumoItens = document.getElementById("resumo-itens");
+
 
 itens.addEventListener("input", () => {
-    resumoItens.innerText = itens.value;
-})
+    if (itens.value.trim() !== "") {
+        resumoItens.innerText = itens.value;
+    } else {
+        resumoItens.innerText = "-";
+    }
+});
 
 
 // MANIPULANDO O FORMULÁRIO PARA ENVIAR AO WHATSAPP
-const formulario = document.getElementById("formulario-frete")
+const formulario = document.getElementById("formulario-frete");
+
 
 formulario.addEventListener("submit", (evento) => {
     evento.preventDefault()
 
-    const origem = document.getElementById("origem")
-    const destino = document.getElementById("destino")
-    const distancia = document.getElementById("distancia")
-    const itens = document.getElementById("itens")
+    const origem = document.getElementById("resumo-origem").innerText;
+    const destino = document.getElementById("resumo-destino").innerText;
+    const distancia = document.getElementById("resumo-distancia").innerText;
+    const itens = document.getElementById("resumo-itens").innerText;
 
-    const mensagem = `Olá Luciano, gostaria de um orçamento!
-    %0A*Retirada:* ${origem}
-    %0A*Destino: ${destino}
-    %0A*Distância: ${distancia}
-    %0A*Itens: ${itens}`
+    const mensagem = `Olá Luciano, Gostaria de solicitar um orçamento! \n
+    *Origem:* ${origem}\n
+    *Destino:* ${destino}\n
+    *Distância:* ${distancia}\n
+    *Itens:* ${itens} `
 
-    const numWhats = "5561984184897"
+    const mensagemFinal = encodeURIComponent(mensagem)
 
-    window.open(`https://wa.me/${numWhats}?text=${mensagem}`, "_blank");
+    const numero = "5561984184897"
+
+    window.open(`https://wa.me/${numero}?text=${mensagemFinal}`, "_blank")
 });
-
